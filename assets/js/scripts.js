c659112f8625a1df423d1f08260103408767134e
ntoniazzi <nicolas@toniazzi.net>
2025-08-29 09:52:30 +0200

    Updated from Publii

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -618,3 +618,11 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 })();
 
+const dateFormater = new Intl.DateTimeFormat("fr",{dateStyle: "full"});
+document.querySelectorAll('time').forEach(e => {
+    const t = e.getAttribute('datetime');
+    if (t === null || t === "" || t === "0") {
+        return;
+    }
+    e.innerHTML = dateFormater.format(new Date(t));
+})
