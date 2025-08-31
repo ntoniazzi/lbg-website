97ba7d308d40c189b4c5752ce23376b5cea1ab28
ntoniazzi <nicolas@toniazzi.net>
2025-08-31 21:29:47 +0200

    Updated from Publii

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -625,4 +625,58 @@ document.querySelectorAll('time').forEach(e => {
         return;
     }
     e.innerHTML = dateFormater.format(new Date(t));
-})
+});
+
+(() => {
+    const form = document.querySelector("form.contact-form");
+    if (null === form) {
+        return;
+    }
+
+    const dialogSuccess = document.getElementById('message-sent');
+    const dialogError = document.getElementById('message-sent');
+    if (null === dialogSuccess || null === dialogError) {
+        return;
+    }
+
+    const button = form.querySelector('[type="submit"]');
+    const label = button.innerHTML;
+        
+    dialogSuccess.addEventListener('close', () => {
+        button.removeAttribute('disabled');
+        button.innerHTML = label;
+        form.reset();
+    });
+
+    dialogError.addEventListener('close', () => {
+        button.removeAttribute('disabled');
+        button.innerHTML = label;
+    });
+
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        button.setAttribute("disabled", "disabled");
+        button.innerHTML = "Envoi en cours…";
+
+        const url = form.getAttribute("action");
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                },
+                body: JSON.stringify({
+                    name: form.elements.name.value,
+                    email: form.elements.email.value,
+                    message: form.elements.message.value,
+                })
+            });
+
+            dialogSuccess.showModal();
+        } catch (error) {
+            dialogError.showModal();
+        };
+    });
+})();
